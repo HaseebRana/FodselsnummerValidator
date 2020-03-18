7a6f3b0df667073296658eb302e00440d9fecdd1
Haseeb Rana <ranahaseeb8@gmail.com>
2020-03-18 17:20:00 +0100

    #3 - Validated Birthdate and if Fødselsnummer has correct number of digits.

--- src/main/java/Validator.java
+++ src/main/java/Validator.java
@@ -1,2 +1,40 @@
 public class Validator {
+
+    public static boolean hasElevenDigits(long number) {
+        int length = (int) (Math.log10(number) + 1);
+        if(length == 11){
+            return true;
+        }
+        return false;
+    }
+
+
+/*    public static boolean hasValidBirthDate(long number) {
+        if(validateBirthDate(number)){
+            return true;
+        }
+        return false;
+    }*/
+
+    public static boolean validateBirthDate(long number){
+        int[] max = {31,12,99};
+
+        boolean isValid = true;
+        int j = 0;
+        for (int i = 0; i < 6; i+=2, j++) {
+
+            int test = (returnPartOfNumber(i,(i+2),number));
+            if(!(test <= max[j])){
+                isValid = false;
+                break;
+            }
+
+        }
+        return isValid;
+
+    }
+
+    public static int returnPartOfNumber(int startIndex, int endIndex, long number) {
+        return Integer.parseInt(Long.toString(number).substring(startIndex,endIndex));
+    }
 }
--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -1,5 +1,78 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
+
+    final long number = 31129966789L;
+
+    @Test
+    void hasElevenDigits(){
+        boolean actual = Validator.hasElevenDigits(number);
+        assertEquals(true, actual);
+    }
+
+    @Test
+    void doesNotHaveElevenDigits(){
+        long number = 1234567890L;
+        boolean actual = Validator.hasElevenDigits(number);
+        assertEquals(false, actual);
+
+    }
+
+/*    @Test
+    void validBirthDate(){
+        boolean actual = Validator.hasValidBirthDate(number);
+        assertEquals(true, actual);
+
+    }*/
+
+    @Test
+    void returnCorrectPartOfNumberFromIndex0to2(){
+        int expected = 31;
+        int actual = Validator.returnPartOfNumber(0,2, number);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void returnCorrectPartOfNumberFromIndex2to4(){
+        int expected = 12;
+        int actual = Validator.returnPartOfNumber(2,4, number);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void returnCorrectPartOfNumberFromIndex0to6(){
+
+        int expected = 311299;
+        int actual = Validator.returnPartOfNumber(0,6, number);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void birthDateIsUnder32AndValid(){
+        boolean actual = Validator.validateBirthDate(number);
+        assertEquals(true, actual);
+
+    }
+    @Test
+    void birthDateIsOver31AndInvalid(){
+        boolean actual = Validator.validateBirthDate(32129966789L);
+        assertEquals(false, actual);
+
+    }
+    @Test
+    void birthMonthIsOver12AndInvalid(){
+        boolean actual = Validator.validateBirthDate(31139966789L);
+        assertEquals(false, actual);
+
+    }
+
+
+
+
+
 
 }
